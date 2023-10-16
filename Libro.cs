2bebdf4e5111ee46e1d4cf52260f133c75510a9f
040701edwin <edwin12073@gmail.com>
2023-10-16 15:33:03 -0600

    Primer commit

---
+++ Libro.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestamos
+{
+    internal class Libro
+    {
+        //Atributos
+        private string _tituloLibro;
+        private string _autor;
+        private int _Nejemplares;
+        private int _NejemplaresPrestados;
+
+        //Metodo constructor por defecto
+        public Libro()
+        {
+
+        }
+        //Metodo constructor por parametros
+        public Libro(string tituloLibro, string autor, int nejemplares, int nejemplaresPrestados)
+        {
+            _tituloLibro = tituloLibro;
+            _autor = autor;
+            _Nejemplares = nejemplares;
+            _NejemplaresPrestados = nejemplaresPrestados;
+        }
+
+        //Metodos getters y setters
+        public string TituloLibro
+        {
+            get { return _tituloLibro; }
+            set { _tituloLibro = value; }
+        }
+        public string autor
+        {
+            get { return _autor; }
+            set { _autor = value; }
+        }
+        public int Nejemplares
+        {
+            get { return _Nejemplares; }
+            set { _Nejemplares = value; }
+        }
+        public int NejemplaresPrestados
+        {
+            get { return _NejemplaresPrestados; }
+            set { _NejemplaresPrestados = value; }
+        }
+        //Metodos
+        public void Pedir(string m, ref int op)
+        {
+            Console.Write("{0}",m);
+            op = int.Parse(Console.ReadLine());
+        }
+        public void Menu()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Opciones Disponibles");
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("1- Prestar");
+            Console.WriteLine("2- Regresar");
+            Console.WriteLine("3- Disponibilidad");
+            Console.WriteLine("4- Salir");
+        }
+        public void Libros()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Libos disponibles");
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("1- Azul");
+            Console.WriteLine("2- La Metamorfosis");
+            Console.WriteLine("4- Salir");
+        }
+        
+
+
+        
+
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using Prestamos;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Reflection.PortableExecutable;
+
+Libro lbr1 = new Libro();
+lbr1.TituloLibro = "Azul";
+lbr1.autor = "Ruben Dario";
+lbr1.Nejemplares = 20;
+
+Libro lbr2 = new Libro("La metamorfosis", "Franz Kaftka", 20, 0);
+
+int op = 0;
+bool band = true;
+
+do
+{
+    lbr1.Menu();
+    lbr1.Pedir("Ingrese la opcion a realizar: ", ref op);
+    Console.Clear();
+    switch (op)
+    {
+        case 1:
+            Console.Clear();
+            lbr1.Libros();
+            lbr1.Pedir("Que libro desea: ", ref op);
+            switch (op)
+            {
+                case 1:
+                    if(lbr1.Nejemplares == 0)
+                        Console.WriteLine("Libro Azul agotado.");
+                    else
+                    {
+                        lbr1.Nejemplares--;
+                        lbr1.NejemplaresPrestados++;
+                    }                    
+                    break;
+                case 2:
+                    if(lbr2.Nejemplares == 0)
+                        Console.WriteLine("Libro Metamorfosis Agotado");
+                    else
+                    {
+                        lbr2.Nejemplares--;
+                        lbr2.NejemplaresPrestados++;
+                    }                    
+                    break;
+                case 3: Environment.Exit(0); break;
+                default: Console.WriteLine("Opcion no valida."); break;
+            }
+            break;
+        case 2:
+            Console.Clear();
+            lbr2.Libros();
+            lbr2.Pedir("Que libro desea: ", ref op);
+            switch (op)
+            {
+                case 1:
+                    if(lbr1.Nejemplares == 20)
+                        Console.WriteLine("No se ha realizado ningun prestamo de este libro.");
+                    else
+                    {
+                        lbr1.Nejemplares++;
+                        lbr1.NejemplaresPrestados--;
+                    }                    
+                    break;
+                case 2:
+                    if (lbr2.Nejemplares == 20)
+                        Console.WriteLine("No se ha realizado ningun prestamo de este libro.");
+                    else
+                    {
+                        lbr2.Nejemplares++;
+                        lbr2.NejemplaresPrestados--;
+                    }
+                    break;
+                case 3: Environment.Exit(0); break;
+                default: Console.WriteLine("Opcion no valida."); break;
+            }
+            break;
+        case 3:
+            Console.Clear();
+            Console.WriteLine("///////////////////////////////////////////");
+            Console.WriteLine("Dispinibilidad de los libros......");
+            Console.WriteLine("Azul: {0}",lbr1.Nejemplares);
+            Console.WriteLine("La Metamorfosiss: {0}", lbr2.Nejemplares);
+            Console.ReadKey();
+            break;
+        case 4:
+            band = false;
+            Console.Clear();
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Opcion incorrecta");
+            Console.WriteLine("--------------------------");
+            break;
+    }
+
+
+} while (band);
